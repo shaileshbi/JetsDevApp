ba8dcd7a41c4b676cf808f96edbd25f859986e6b
shaileshbi <143604344+shaileshbi@users.noreply.github.com>
2023-08-30 16:18:40 +0530

    use comment in reducer

--- app/store/reducers/favorite-user.reducer.ts
+++ app/store/reducers/favorite-user.reducer.ts
@@ -7,19 +7,23 @@ interface favUser{
 const initialState:favUser = {
    users:[]
 }
+/** favorite user */
 export const favoriteUserSlice = createSlice({
     name:'favoriteUser',
     initialState,
     reducers:{
+          /** set user data */
         setUsers:(state:favUser,action:PayloadAction<UserResType>)=>{
         return {users:action.payload}
         },
+         /** call for favorite */
         setUserFavorite:(state:favUser,action:PayloadAction<UserResType>)=>{
             let favoriteItem = [...state?.users];
             let index = state?.users?.findIndex((el:UserResType) => el?.login?.username === action.payload);
             favoriteItem[index] = {...favoriteItem[index], status: true}; 
             return  {...state,users:favoriteItem}
         },
+        /** call for unfavorite */
         setUserUnFavorite:(state:favUser,action:PayloadAction<UserResType>)=>{
             let unFavoriteItem = [...state?.users]; 
             let index = state?.users.findIndex((el:UserResType) => el?.login?.username === action.payload);
